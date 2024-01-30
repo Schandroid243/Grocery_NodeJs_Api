b9b090f614f3f748e3b6e9d76b866bf0631c3562
Schandroid243 <schadrackngunza@gmail.com>
2024-01-30 13:28:13 +0100

    feat(user.controller): Added two methods which are login and register
    feat(auth): The middleware is used to generate a new token and to verify if the submitted token is valid
    
    feat(user.model): Contains the user model
    
    feat(app.routes): Added user routes "login" and "register"
    
    feat(users.service): Added two function which are login and register

---
+++ config/configToken.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN_KEY: "the_quest_of_sir_francis_drake_leads_us_to_the_random_key",
+  refreshTokenSecret: "some-secret-refresh-token-shit",
+  tokenLife: 86400,
+  refreshTokenLife: 86400,
+};
