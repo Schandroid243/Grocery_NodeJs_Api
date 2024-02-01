074cb9c68a5a765ae51a67b3809b6716661b9df5
Schandroid243 <schadrackngunza@gmail.com>
2024-02-01 04:47:59 +0100

    commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(errors.errorHandler);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
-const IP = process.env.IP || "192.168.18.13";
+const IP = process.env.IP || "192.168.91.193";
 
 app.listen(PORT, IP, () => {
   console.log(`listening on port:  ${PORT} and ip: ${IP}`);
